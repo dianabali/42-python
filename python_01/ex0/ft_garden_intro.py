e25c8827bbe356a345d75212689cb66aac5ab506
Diana <dianabali64@gmail.com>
2026-07-25 12:19:25 +0200

    python 01

---
+++ python_01/ex0/ft_garden_intro.py
@@ -0,0 +1,13 @@
+def main():
+    name = "Rose"
+    height = 25
+    age = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {name}")
+    print(f"Age: {name}")
+    print("=== End of Program ===")
+    
+if __name__ == "__main__":
+    main()
