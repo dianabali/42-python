58e2af896878b42f41a4bcbfa52218a8d98bb4b1
Diana <dianabali64@gmail.com>
2026-08-04 12:05:04 +0200

    ex6

---
+++ python_03/ex6/ft_data_alchemist.py
@@ -0,0 +1,68 @@
+"""
+List comprehensions - a short way to create a new list from an existing iterable.
+Syntax: my_list = [expression for item in iterable]
+Example w/o:
+    numbers = [1, 2, 3, 4, 5]
+    squares = []
+    for number in numbers:
+        squares.append(number * number)
+    print(squares)
+Example w:
+    numbers = [1, 2, 3, 4, 5]
+    squares = [number * number for number in numbers]
+    print(squares)
+
+
+Dictionary comprehensions - dictionary version of list comprehensions.
+Syntax: new_dict = {key_expression: value_expression for item in iterable}
+Example w/o:
+    players = ["Alice", "Bob", "Charlie"]
+    scores = {}
+    for player in players:
+        scores[player] = 0
+    print(scores)
+Example w:
+    scores = {player: 0 for player in players}
+"""
+
+import random
+
+print("=== Game Data Alchemist ===")
+
+print()
+
+players = [
+    "Alice",
+    "bob",
+    "Charlie",
+    "dylan",
+    "Emma",
+    "Gregory",
+    "john",
+    "kevin",
+    "Liam"
+]
+
+print("Initial list of players:", players)
+
+capitalized_players = [name.capitalize() for name in players]
+
+print("New list with all names capitalized:", capitalized_players)
+
+only_capitalized_players = [name for name in players if name[0].isupper()]
+
+print("New list of capitalized names only:", only_capitalized_players)
+
+scores = {name: random.randint(1, 1000) for name in capitalized_players}
+
+print()
+
+print("Score dict:", scores)
+
+average = round(sum(scores.values()) / len(scores), 2)
+
+print("Score average is", average)
+
+high_scores = {name: score for name, score in scores.items() if score > average}
+
+print("High scores:", high_scores)
