437eee8da942493c03f00b18e4e21941c103fb53
Diana <dianabali64@gmail.com>
2026-08-07 16:38:27 +0200

    ex1

--- python_04/ex1/ft_archive_creation.py
+++ python_04/ex1/ft_archive_creation.py
@@ -1,1 +1,57 @@
 import sys
+from typing import IO
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_archive_creation.py <file>")
+        return
+
+    filename = sys.argv[1]
+
+    print("=== Cyber Archives Recovery & Preservation ===")
+    print(f"Accessing file '{filename}'")
+
+    try:
+        file: IO[str] = open(filename, "r")
+        content = file.read()
+
+        print("---")
+        print()
+        print(content)
+        print()
+        print("---")
+
+        file.close()
+        print(f"File '{filename}' closed.")
+
+        print()
+        print("Transform data:")
+
+        transformed = ""
+        for line in content.splitlines():
+            transformed += line + "#\n"
+
+        print("---")
+        print()
+        print(transformed)
+        print("---")
+
+        new_filename = input("Enter new file name (or empty): ")
+
+        if new_filename == "":
+            print("Not saving data.")
+        else:
+            print(f"Saving data to '{new_filename}'")
+
+            output: IO[str] = open(new_filename, "w")
+            output.write(transformed)
+            output.close()
+
+            print(f"Data saved in file '{new_filename}'.")
+    except OSError as error:
+        print(f"Error opening file '{filename}': {error}")
+
+
+if __name__ == "__main__":
+    main()
